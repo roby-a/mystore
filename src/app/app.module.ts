b5ad1559d5ed66645635e8b268c855503bfcda05
roby <roby.a@experionglobal.com>
2019-11-08 09:41:55 +0530

    Added files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContainComponent } from './contain/contain.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/product/product.ts
@@ -0,0 +1,8 @@
+export interface Products {
+    name: string;
+    image: string;
+    price: number;
+    description: string;
+    imageAlt: string;
+    isAvailable: boolean;
+}
